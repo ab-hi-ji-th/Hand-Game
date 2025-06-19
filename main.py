d42974940cdea3fce7558bef3ecc05d4f5d57725
Abhijith <abhijithpv32@gmail.com>
2025-06-19 20:11:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from PyQt5.QtWidgets import QApplication
 from start_menu import StartMenu
 
 if __name__ == '__main__':
-    app = QApplication([])
+    app = QApplication([]) 
     window = StartMenu()
     window.show()
     app.exec_()
